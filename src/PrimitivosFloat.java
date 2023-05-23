3662d2d5a37b0a4099d603d007c03948d78a95cc
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-05-23 10:20:05 -0400

    Primer commit

---
+++ src/PrimitivosFloat.java
@@ -0,0 +1,34 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat =  3.4028235E38F;
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("float corresponde en byte a = " + Float.BYTES);
+        System.out.println("float corresponde en bites a = " + Float.SIZE);
+        System.out.println("maximo valor para float = " + Float.MAX_VALUE);
+        System.out.println("minimo valor para float = " + Float.MIN_VALUE);
+
+        double realDouble =  1.7976931348623157E308D;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("double corresponde en byte a = " + Double.BYTES);
+        System.out.println("double corresponde en bites a = " + Double.SIZE);
+        System.out.println("maximo valor para double = " + Double.MAX_VALUE);
+        System.out.println("minimo valor para double = " + Double.MIN_VALUE);
+
+        float varFlotante = 3.1416f;
+        System.out.println("varFlotante = " + varFlotante);
+
+        /*
+        * git init
+        * git status
+        * git add
+        * git add .
+        * git commit -m ""
+        * git branch -M main
+        * git remote add origin https://github.com/LuisWork/Repositorio.git
+        * git branch -M main
+        * git push -u origin main
+        *
+        * */
+    }
+}
